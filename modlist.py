c1d417051d42a8767fac7aea97be3dc17a34634b
bob <dariboufix@daribouca.net>
2013-06-02 13:59:28 +0200

    See details
    - added "version" & "desc"
    - added Bob's huey skin

--- modlist.py
+++ modlist.py
@@ -3,6 +3,18 @@ __author__ = 'bob'
 mods = {
     "mod-exports": {
         "remote": "https://github.com/TDC-bob/mod-exports.git",
-        "branch": "master"
+        "branch": "master",
+        "version": 1,
+        "desc": "Exports des différents affichages (CDU, RWR, horloge, etc ...) pour les afficher"
+                "sur un second écran"
+    }
+}
+
+skins = {
+    "TDC-Bob-huey": {
+        "remote": "https://github.com/TDC-bob/skin-huey-bob.git",
+        "branch": "master",
+        "version": 1,
+        "desc": "Skin créée par Bob"
     }
 }
